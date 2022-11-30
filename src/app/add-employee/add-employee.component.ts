dc5ac12b44d512f994eb45792189f2577fd29a86
irshad007 <irshadmuhammedch8@gmail.com>
2022-11-30 17:44:45 +0530

    add employee

---
+++ src/app/add-employee/add-employee.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-employee',
+  templateUrl: './add-employee.component.html',
+  styleUrls: ['./add-employee.component.css']
+})
+export class AddEmployeeComponent {
+  EmpId=""
+  FirstName=""
+  HouseNO=""
+  LastName=""
+  houseNAme=""
+  StreetName=""
+  Pin=""
+  District=""
+  State=""
+  Country=""
+  Mob=""
+  Email=""
+  ParentName=""
+  Gender=""
+  HighestDegree=""
+  YearExp=""
+  dofJoin=""
+  Username=""
+  password=""
+  confPassword=""
+
+
+}
